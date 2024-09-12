4b17397aee6e915c549feb1e42f9945d21d0df28
boyarinovLex <boyarinovLex@ya.ru>
2024-09-12 17:56:20 +0700

    Refactoring 'upload' method from io to nio

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,8 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
@@ -25,14 +23,19 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
+        Path pathToDir = Paths.get(directoryPath).normalize();
+        try {
+            if (Files.notExists(pathToDir)) {
+                Files.createDirectories(pathToDir);
+            }
+            Path file = Files.createFile(Paths.get(directoryPath + fileName));
+            try (OutputStream outputStream = Files.newOutputStream(file)) {
+                outputStream.write(multipartFile.getBytes());
             } catch (IOException ex) {
                 throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
